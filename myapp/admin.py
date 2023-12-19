374aca870e72702afc066eaf352415307e9d09c7
SravyaBhamidipalli <150989743+SravyaBhamidipalli@users.noreply.github.com>
2023-12-19 09:29:04 +0530

    Update admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
-from myapp.models import Product,Cart,Buy,Wishlist,Reply,Category,FAQ,ContactMessage
+from myapp.models import Product,Cart,Buy,Reply,Category,FAQ,ContactMessage
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Cart) 
 admin.site.register(Buy)
-admin.site.register(Wishlist)
 admin.site.register(Reply)
 admin.site.register(Category)
-#admin.site.register(ProductReply)
 admin.site.register(FAQ)
 admin.site.register(ContactMessage)
 
